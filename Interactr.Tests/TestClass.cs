9540f941625bf33512b68588b1dd00ff48613a40
Wouter <wouterdekrsmckr@gmail.com>
2018-02-28 15:51:45 +0100

    Added travis setup and modified solution setup

---
+++ Interactr.Tests/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.Pass("My first test");
+        }
+
+        [Test]
+        public void TestFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
