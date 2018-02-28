1e4148ef70b6d63ec203d2c78101f905b71458a6
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-02-28 23:26:16 +0100

    Implement the basic structure of the message and party class.

---
+++ Interactr/Model/Message.cs
@@ -0,0 +1,72 @@
+﻿namespace Interactr.Model
+{
+    /// <summary>
+    /// Represent a message passed between parties.
+    /// </summary>
+    public class Message
+    {
+        private Party _sender;
+        private Party _receiver;
+        private MessageType _type;
+        private string _label;
+
+        public Message(Party sender, Party receiver, MessageType type, string label)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Type = type;
+            Label = label;
+        }
+
+        /// <summary>
+        /// Enum that represents the type of a message.
+        /// </summary>
+        public enum MessageType
+        {
+            Invocation,
+            Result
+        }
+
+        /// <summary>
+        /// Represent the type the message.
+        /// </summary>
+        /// <remarks>
+        /// It is impossible for a message to not have a type.
+        /// </remarks>
+        public MessageType Type
+        {
+            get => _type;
+            set => _type = value;
+        }
+
+        /// <summary>
+        /// Represent the message label.
+        /// </summary>
+        /// <remarks>
+        /// There are no restrictions on the format of the label.
+        /// </remarks>
+        public string Label
+        {
+            get => _label;
+            set => _label = value;
+        }
+
+        /// <summary>
+        /// Represent the message sender.
+        /// </summary>
+        public Party Sender
+        {
+            get => _sender;
+            set => _sender = value;
+        }
+
+        /// <summary>
+        /// Represent the message receiver.
+        /// </summary>
+        public Party Receiver
+        {
+            get => _receiver;
+            set => _receiver = value;
+        }
+    }
+}
---
+++ Interactr/Model/Party.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Interactr.Model
+{
+    /// <summary>
+    /// Represents an Actor or an Object in the diagram.
+    /// </summary>
+    public class Party
+    {
+        private string _label;
+        private PartyType _type;
+
+        public Party(PartyType type, string label)
+        {
+            Type = type;
+            Label = label;
+        }
+
+        /// <summary>
+        /// Enum that represents the type of a Party.
+        /// </summary>
+        public enum PartyType
+        {
+            Actor,
+            Object
+        }
+
+        /// <summary>
+        /// Represent the type of this party.
+        /// </summary>
+        /// <remarks>
+        /// It is impossible for a party to not have a type.
+        /// </remarks>
+        public PartyType Type
+        {
+            get => _type;
+            set => _type = value;
+        }
+
+        /// <summary> A label in the specified format.
+        /// <example> instance_name;class_name </example>
+        /// </summary>
+        //TODO validate label before assignment.
+        public string Label
+        {
+            get => _label;
+            set => _label = value;
+        }
+
+    }
+}
