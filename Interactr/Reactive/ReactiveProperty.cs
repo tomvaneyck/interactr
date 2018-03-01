5a32cf79549b10467b35ab0e25c3f775fea5deae
Wouter <wouterdekrsmckr@gmail.com>
2018-03-01 16:59:59 +0100

    Added Changing observable, lazy initialization, prepend Changing with current value

--- Interactr/Reactive/ReactiveProperty.cs
+++ Interactr/Reactive/ReactiveProperty.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,41 @@ namespace Interactr.Reactive
     public class ReactiveProperty<T>
     {
         private T _value;
-        
-        private readonly Subject<T> _changed = new Subject<T>();
-        public IObservable<T> Changed => _changed;
+
+        private Subject<T> _changing;
+        public IObservable<T> Changing
+        {
+            get
+            {
+                if (_changing == null)
+                {
+                    _changing = new Subject<T>();
+                }
+                return _changing;
+            }
+        }
+
+        private Subject<T> _changed;
+        public IObservable<T> Changed
+        {
+            get
+            {
+                if (_changed == null)
+                {
+                    _changed = new Subject<T>();
+                }
+                return _changed.StartWith(Value);
+            }
+        }
 
         public T Value
         {
             get => _value;
             set
             {
+                _changing?.OnNext(_value);
                 _value = value;
-                _changed.OnNext(_value);
+                _changed?.OnNext(_value);
             }
         }
     }
