0d27870edda00addd08cbf8270d97d3938e93f08
Wouter <wouterdekrsmckr@gmail.com>
2018-03-01 16:48:02 +0100

    First observable tests

---
+++ Interactr/Reactive/ReactiveProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.Reactive
+{
+    public class ReactiveProperty<T>
+    {
+        private T _value;
+        
+        private readonly Subject<T> _changed = new Subject<T>();
+        public IObservable<T> Changed => _changed;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                _changed.OnNext(_value);
+            }
+        }
+    }
+}
