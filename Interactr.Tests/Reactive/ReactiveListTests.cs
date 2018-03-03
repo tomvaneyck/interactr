82d2127b0a92e703383ba4dfd85f29634e35dae8
Wouter <wouterdekrsmckr@gmail.com>
2018-03-03 12:12:23 +0100

    Added Microsoft.Reactive.Testing library, added testing classes for ReactiveList and ReactiveProperty

---
+++ Interactr.Tests/Reactive/ReactiveListTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interactr.Reactive;
+using Microsoft.Reactive.Testing;
+
+namespace Interactr.Tests.Reactive
+{
+    [TestFixture]
+    public class ReactiveListTests
+    {
+        [Test]
+        public void TestAdd()
+        {
+            
+        }
+    }
+}
---
+++ Interactr.Tests/Reactive/ReactivePropertyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.Tests.Reactive
+{
+    [TestFixture]
+    public class ReactivePropertyTests
+    {
+    }
+}
