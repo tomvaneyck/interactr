ef5f5f2f7b780b3fc5b60898faa075e1f40492c5
Wouter <wouterdekrsmckr@gmail.com>
2018-03-04 17:05:01 +0100

    Added ReactiveDictionary, added ObserveEach to ReactiveList, added ObserveNested

---
+++ Interactr/Reactive/ReactiveExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.Reactive
+{
+    public static class ReactiveExtensions
+    {
+        /// <summary>
+        /// Takes IObservable&lt;T&gt;, projects each element of type T on an IObservable&lt;R&gt; 
+        /// and returns the values of type R emitted by the most recently emitted T.
+        /// If a null value is encountered for T, this value is simply ignored.
+        /// </summary>
+        /// <typeparam name="T">The source observable type</typeparam>
+        /// <typeparam name="R">The observable type nested inside T</typeparam>
+        /// <param name="obs">The source observable</param>
+        /// <param name="subObsSelector">Projection function of T onto an IObservable&lt;R&gt;</param>
+        /// <returns>A new observable of type R.</returns>
+        public static IObservable<R> ObserveNested<T, R>(this IObservable<T> obs, Func<T, IObservable<R>> subObsSelector)
+        {
+            return obs.Where(t => t != null).Select(subObsSelector).Switch();
+        }
+    }
+}
