972dbfa95a1487f10f9c5a2497a4c47b41e489bf
Tom Van Eyck <tom.vaneyck@telenet.be>
2018-03-04 01:21:48 +0100

    Changed regex function and CamelCased IsValidLabel

--- Interactr.Tests/Model/EmptyClass.cs
+++ Interactr.Tests/Model/EmptyClass.cs
@@ -11,84 +11,84 @@ namespace Interactr.Tests.Model
         public void Test_1()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("A;B"));
+            Assert.AreEqual(false, p.IsValidLabel("A;B"));
            
         }
         [Test]
         public void Test_2()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("a;b"));
+            Assert.AreEqual(false, p.IsValidLabel("a;b"));
 
         }
         [Test]
         public void Test_3()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("A;b"));
+            Assert.AreEqual(false, p.IsValidLabel("A;b"));
 
         }
         [Test]
         public void Test_4()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(true, p.isValidLabel("a;B"));
+            Assert.AreEqual(true, p.IsValidLabel("a;B"));
 
         }
         [Test]
         public void Test_5()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel(";b"));
+            Assert.AreEqual(false, p.IsValidLabel(";b"));
 
         }
         [Test]
         public void Test_6()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(true, p.isValidLabel("a;Bc"));
+            Assert.AreEqual(true, p.IsValidLabel("a;Bc"));
         }
         [Test]
         public void Test_7()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("A;BC"));
+            Assert.AreEqual(false, p.IsValidLabel("A;BC"));
         }
         [Test]
         public void Test_8()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel(";"));
+            Assert.AreEqual(false, p.IsValidLabel(";"));
         }
         [Test]
         public void Test_9()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("a;"));
+            Assert.AreEqual(false, p.IsValidLabel("a;"));
         }
         [Test]
         public void Test_10()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("A;"));
+            Assert.AreEqual(false, p.IsValidLabel("A;"));
         }
         [Test]
         public void Test_11()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(true, p.isValidLabel("test;Test"));
+            Assert.AreEqual(true, p.IsValidLabel("test;Test"));
         }
         [Test]
         public void Test_12()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("Hey;Hey"));
+            Assert.AreEqual(false, p.IsValidLabel("Hey;Hey"));
         }
         [Test]
         public void Test_13()
         {
             Party p = new Party(Party.PartyType.Object, "a");
-            Assert.AreEqual(false, p.isValidLabel("hi;hi"));
+            Assert.AreEqual(false, p.IsValidLabel("hi;hi"));
         }
     }
 }
--- Interactr/Model/Party.cs
+++ Interactr/Model/Party.cs
@@ -33,15 +33,36 @@ namespace Interactr.Model
         public PartyType Type { get; set; }
 
         /// <summary> A label in the specified format.
-        /// <example> instance_name;class_name </example>
+        /// <example> [instance_name]:class_name </example>
         /// </summary>
+        /// <remarks>
+        /// The label is only accepted if the optional instance_name starts with lowercase,
+        /// has one colon followed by a class_name starting with uppercase.
+        /// The label is thus only accepted if it follows on of the following patterns:
+        /// <list type="bullet">
+        /// <item>test:Approved</item>
+        /// <item>:Approved</item>
+        /// </list>
+        /// and rejected when it follows one of the following patterns:
+        /// <list type="bullet">
+        /// <item>Test:Rejected</item>
+        /// <item>test:rejected</item>
+        /// <item>test:</item>
+        /// </list>
+        /// </remarks>
         //TODO validate label before assignment.
         public string Label { get; set; }
 
-        public bool isValidLabel(string label)
+        public bool IsValidLabel(string label)
         {
-            string pattern = @"(^;|^[a-z]\w*;)[A-Z]\w*";
-            return Regex.IsMatch(label,pattern);
+            if (Regex.Match(label, "^(([a-z\u00C0-\u017F]{1}[a-zA-Z0-9\u00C0-\u017F]*)?:){1}([A-Z\u00C0-\u017F]{1}[a-zA-Z0-9\u00C0-\u017F]*)+$").Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 
