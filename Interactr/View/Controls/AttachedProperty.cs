079e99a317f1481bfa81d0a0a6b1631d3e4a5cdb
Wouter <wouterdekrsmckr@gmail.com>
2018-03-05 14:15:17 +0100

    Added documentation to AttachedProperty

--- Interactr/View/Controls/AttachedProperty.cs
+++ Interactr/View/Controls/AttachedProperty.cs
@@ -20,20 +20,39 @@ namespace Interactr.View.Controls
     /// </summary>
     public class AttachedProperty
     {
+        /// <summary>
+        /// Type of the property values.
+        /// </summary>
         public Type Type { get; }
-        public object DefaultValue { get; }
 
-        public AttachedProperty(Type type, object defaultValue)
+        /// <summary>
+        /// The default value for this property.
+        /// If the property is not explicitly set, this value will be used.
+        /// </summary>
+        public object DefaultValueObject { get; }
+
+        /// <summary>
+        /// Creates a new attached property.
+        /// </summary>
+        /// <param name="type">The type of value stored in this property.</param>
+        /// <param name="defaultValueObject">The default value to be set for this property.</param>
+        public AttachedProperty(Type type, object defaultValueObject)
         {
-            if (!type.IsInstanceOfType(defaultValue) && defaultValue != null)
+            if (!type.IsInstanceOfType(defaultValueObject) && defaultValueObject != null)
             {
                 throw new ArgumentException("Invalid default value");
             }
 
             Type = type;
-            this.DefaultValue = defaultValue;
+            this.DefaultValueObject = defaultValueObject;
         }
 
+        /// <summary>
+        /// Retrieve the value for this property on the given element, 
+        /// or DefaultValueObject if none has been set.
+        /// </summary>
+        /// <param name="elem">The element to retrieve the value from.</param>
+        /// <returns>The property value.</returns>
         public object GetValueObject(UIElement elem)
         {
             if (elem.AttachedProperties.TryGetValue(this, out object value))
@@ -41,9 +60,17 @@ namespace Interactr.View.Controls
                 return value;
             }
 
-            return DefaultValue;
+            return DefaultValueObject;
         }
 
+        /// <summary>
+        /// Set the value for this property on the given element.
+        /// </summary>
+        /// <param name="elem">
+        /// The new value for this property on the given object. 
+        /// The value type must match the type of this property.
+        /// </param>
+        /// <param name="value">The value to set.</param>
         public void SetValueObject(UIElement elem, object value)
         {
             if (!Type.IsInstanceOfType(value) && value != null)
@@ -55,16 +82,40 @@ namespace Interactr.View.Controls
         }
     }
 
+    /// <summary>
+    /// An attachedproperty with a generic type for the property value
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in this property</typeparam>
     public class AttachedProperty<T> : AttachedProperty
     {
-        public AttachedProperty(T defaultValue) : base(typeof(T), defaultValue)
+        /// <summary>
+        /// The default value for this property.
+        /// If the property is not explicitly set, this value will be used.
+        /// </summary>
+        public T DefaultValue => (T) DefaultValueObject;
+
+        public AttachedProperty(T defaultValueObject) : base(typeof(T), defaultValueObject)
         {}
 
+        /// <summary>
+        /// Retrieve the value for this property on the given element, 
+        /// or DefaultValueObject if none has been set.
+        /// </summary>
+        /// <param name="elem">The element to retrieve the value from.</param>
+        /// <returns>The property value.</returns>
         public T GetValue(UIElement elem)
         {
             return (T)GetValueObject(elem);
         }
 
+        /// <summary>
+        /// Set the value for this property on the given element.
+        /// </summary>
+        /// <param name="elem">
+        /// The new value for this property on the given object. 
+        /// The value type must match the type of this property.
+        /// </param>
+        /// <param name="value">The value to set.</param>
         public void SetValue(UIElement elem, T value)
         {
             elem.AttachedProperties[this] = value;
