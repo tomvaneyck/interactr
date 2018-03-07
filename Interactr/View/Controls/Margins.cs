ff788899b686602f5a77522587739e1a168cbddf
Tom Van Eyck <tom.vaneyck@telenet.be>
2018-03-07 14:26:06 +0100

    Added comments and documentation to Margin

--- Interactr/View/Controls/Margins.cs
+++ Interactr/View/Controls/Margins.cs
@@ -1,5 +1,8 @@
 ﻿namespace Interactr.View.Controls
 {
+    /// <summary>
+    /// Hold the size of the margins around the 4 sides.
+    /// </summary>
     public struct Margins
     {
         public int Left { get; }
@@ -15,11 +18,23 @@
             this.Bottom = bottom;
         }
 
+        /// <summary>
+        /// See if the two given margins have different values.
+        /// </summary>
+        /// <param name="m1">First margin</param>
+        /// <param name="m2">Second margin</param>
+        /// <returns>True if the margins are not equal, otherwise false.</returns>
         public static bool operator !=(Margins m1, Margins m2)
         {
             return !(m1 == m2);
         }
 
+        /// <summary>
+        /// See if the two given margins have the same values.
+        /// </summary>
+        /// <param name="m1">First margin</param>
+        /// <param name="m2">Second margin</param>
+        /// <returns>True if the margins are equal, otherwise false.</returns>
         public static bool operator ==(Margins m1, Margins m2)
         {
             return m1.Left == m2.Left &&
@@ -28,11 +43,20 @@
                    m1.Bottom == m2.Bottom;
         }
 
+        /// <summary>
+        /// Convert margin to readable format.
+        /// </summary>
+        /// <returns>The string formatted for human reading.</returns>
         public override string ToString()
         {
             return $"({this.Left}, {this.Top}, {this.Right}, {this.Bottom})";
         }
 
+        /// <summary>
+        /// See if given object is the same as this.
+        /// </summary>
+        /// <param name="obj">The given object.</param>
+        /// <returns>True if the given object and this are equal, otherwise false.</returns>
         public override bool Equals(object obj)
         {
             // Check for null values and compare run-time types.
@@ -43,6 +67,10 @@
             return this == m;
         }
 
+        /// <summary>
+        /// Return the hashcode for this object.
+        /// </summary>
+        /// <returns>The hashcode for this object.</returns>
         public override int GetHashCode()
         {
             return Left ^ Top ^ Right ^ Bottom;
