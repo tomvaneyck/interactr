1c0c9dde932f4dbd3d02a1c94e7674b60e1682de
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-03-08 12:01:58 +0100

    Add comments in partyview.

--- Interactr/View/PartyView.cs
+++ Interactr/View/PartyView.cs
@@ -15,41 +15,50 @@ using Point = Interactr.View.Framework.Point;
 
 namespace Interactr.View
 {
+    /// <summary>
+    /// A view for one party.
+    /// </summary>
     public class PartyView : AnchorPanel
     {
         #region ViewModel
+
         private readonly ReactiveProperty<PartyViewModel> _viewModel = new ReactiveProperty<PartyViewModel>();
+
         public PartyViewModel ViewModel
         {
             get => _viewModel.Value;
             set => _viewModel.Value = value;
         }
+
         public IObservable<PartyViewModel> ViewModelChanged => _viewModel.Changed;
+
         #endregion
-        
+
         private readonly ImageView _actorImage = new ImageView();
         private readonly RectangleView _objectRectangle = new RectangleView();
         private readonly LabelView _labelView = new LabelView();
 
         public PartyView()
         {
-            //Set size of this control
+            // Set size of this control
             PreferredWidth = 125;
             PreferredHeight = 150;
 
-            //Set layout
+            // Set the image
             _actorImage.Image = Resources.StickFigure;
+
+            // Set layout
             MarginsProperty.SetValue(_actorImage, new Margins(0, 0, 0, 25));
             MarginsProperty.SetValue(_objectRectangle, new Margins(0, 0, 0, 25));
             _labelView.Position = new Point(0, 125);
             AnchorsProperty.SetValue(_labelView, Anchors.Bottom);
 
-            // Display the view that matches the party type
+            // Define the display to be the view that matches the party type
             ViewModelChanged.ObserveNested(vm => vm.TypeChanged).Subscribe(partyType =>
-                {
-                    _actorImage.IsVisible = partyType == Party.PartyType.Actor;
-                    _objectRectangle.IsVisible = partyType == Party.PartyType.Object;
-                });
+            {
+                _actorImage.IsVisible = partyType == Party.PartyType.Actor;
+                _objectRectangle.IsVisible = partyType == Party.PartyType.Object;
+            });
 
             // Bi-directional bind party label to view
             ViewModelChanged.ObserveNested(vm => vm.LabelChanged)
@@ -63,7 +72,8 @@ namespace Interactr.View
             Observable.Merge(
                     _objectRectangle.MouseEventOccured,
                     _actorImage.MouseEventOccured
-                ).Where(e => e.Id == MouseEvent.MOUSE_CLICKED && e.ClickCount % 2 == 0) // Modulo for consequent double clicks.
+                ).Where(e =>
+                    e.Id == MouseEvent.MOUSE_CLICKED && e.ClickCount % 2 == 0) // Modulo for consequent double clicks.
                 .Subscribe(_ => ViewModel?.TogglePartyType());
 
             // Add child elements
