4dabc2513128491e9bb6a7bb905a5ca392d3972d
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-03-08 17:25:46 +0100

    First style changes.

--- Interactr/View/Controls/LabelView.cs
+++ Interactr/View/Controls/LabelView.cs
@@ -14,57 +14,72 @@ using Interactr.Window;
 namespace Interactr.View.Controls
 {
     /// <summary>
-    /// A view for displaying and editing text.
+    /// A view for displaying and aediting the text label.
     /// </summary>
     public class LabelView : UIElement
     {
         #region Text
-        /// <summary>
-        /// The text that is displayed.
-        /// </summary>
+
+        private readonly ReactiveProperty<string> _text = new ReactiveProperty<string>();
+
         public string Text
         {
             get => _text.Value;
             set => _text.Value = value;
         }
-        private readonly ReactiveProperty<string> _text = new ReactiveProperty<string>();
+
         public IObservable<string> TextChanged => _text.Changed;
+
         #endregion
 
         #region Font
-        /// <summary>
-        /// The font used to render the label.
-        /// </summary>
+
+        private readonly ReactiveProperty<Font> _font = new ReactiveProperty<Font>();
+
         public Font Font
         {
             get => _font.Value;
             set => _font.Value = value;
         }
-        private readonly ReactiveProperty<Font> _font = new ReactiveProperty<Font>();
+
         public IObservable<Font> FontChanged => _font.Changed;
+
         #endregion
 
         #region IsInEditMode
+
+        private readonly ReactiveProperty<bool> _isInEditMode = new ReactiveProperty<bool>();
+
         public bool IsInEditMode
         {
             get => _isInEditMode.Value;
             set => _isInEditMode.Value = value;
         }
-        private readonly ReactiveProperty<bool> _isInEditMode = new ReactiveProperty<bool>();
+
         public IObservable<bool> EditModeChanged => _isInEditMode.Changed;
+
         #endregion
 
         #region CanLeaveEditMode
+
+        private readonly ReactiveProperty<bool> _canLeaveEditMode = new ReactiveProperty<bool>();
+
         public bool CanLeaveEditMode
         {
             get => _canLeaveEditMode.Value;
             set => _canLeaveEditMode.Value = value;
         }
-        private readonly ReactiveProperty<bool> _canLeaveEditMode = new ReactiveProperty<bool>();
+
         public IObservable<bool> CanLeaveEditModeChanged => _canLeaveEditMode.Changed;
+
         #endregion
 
-        private bool _cursorIsVisible = false;
+        #region CursorIsVisible
+
+        private bool _cursorIsVisible;
+
+        #endregion
+
 
         public LabelView()
         {
@@ -72,8 +87,7 @@ namespace Interactr.View.Controls
             Font = new Font("Arial", 11);
 
             // When a property changes, repaint.
-            Observable.Merge(
-                TextChanged.Select(_ => Unit.Default),
+            TextChanged.Select(_ => Unit.Default).Merge(
                 FontChanged.Select(_ => Unit.Default)
             ).Subscribe(_ => Repaint());
 
@@ -82,43 +96,43 @@ namespace Interactr.View.Controls
             {
                 if (editMode)
                 {
-                    return Observable.Interval(TimeSpan.FromMilliseconds(SystemInformation.CaretBlinkTime)).StartWith(0);
+                    return Observable.Interval(TimeSpan.FromMilliseconds(SystemInformation.CaretBlinkTime))
+                        .StartWith(0);
                 }
-                else
-                {
-                    _cursorIsVisible = false;
-                    return Observable.Empty<long>();
-                }
+
+                _cursorIsVisible = false;
+                return Observable.Empty<long>();
             }).Switch().Subscribe(_ =>
             {
                 _cursorIsVisible = !_cursorIsVisible;
                 Repaint();
             });
 
-            // Block focus change if not allowed to leave edit mode and repaint.
+            // Leave edit mode if focus is lost and Repaint.
             FocusChanged.Subscribe(isFocused =>
             {
                 if (!isFocused)
                 {
                     IsInEditMode = false;
                 }
+
                 Repaint();
             });
 
-            // Block focus losing when canLeaveEditMode false.
+            // Update canLoseFocus when the CanLeaveEditMode is changed.
             CanLeaveEditModeChanged.Subscribe(canLoseFocus => CanLoseFocus = canLoseFocus);
-
             CanLeaveEditMode = true;
         }
 
+        /// <inheritdoc cref="PaintElement"/>
         public override void PaintElement(Graphics g)
         {
             // Measure how much space it would take to fully render the
             // the string. Must be done in this function because it requires
             // a Graphics object.
             var preferredSize = g.MeasureString(Text, Font);
-            PreferredWidth = (int)Math.Ceiling(preferredSize.Width);
-            PreferredHeight = (int)Math.Ceiling(preferredSize.Height);
+            PreferredWidth = (int) Math.Ceiling(preferredSize.Width);
+            PreferredHeight = (int) Math.Ceiling(preferredSize.Height);
 
             if (IsFocused)
             {
@@ -135,6 +149,7 @@ namespace Interactr.View.Controls
             }
         }
 
+        /// <inheritdoc cref="OnKeyEvent"/>
         protected override bool OnKeyEvent(KeyEventData eventData)
         {
             if (eventData.KeyCode == KeyEvent.VK_ESCAPE)
@@ -143,28 +158,33 @@ namespace Interactr.View.Controls
                 {
                     IsInEditMode = false;
                 }
+
                 return true;
             }
 
             if (eventData.Id == KeyEvent.KEY_TYPED && IsInEditMode)
             {
-                if (eventData.KeyChar == '\b') //Backspace
+                // If the keyChar is backspace.
+                if (eventData.KeyChar == '\b')
                 {
                     if (Text.Length > 0)
                     {
                         Text = Text.Substring(0, Text.Length - 1);
                     }
                 }
-                else if (eventData.KeyChar != '\x1b') //Not escape
+                // If Keychar is not escape.
+                else if (eventData.KeyChar != '\x1b')
                 {
                     Text += eventData.KeyChar;
                 }
+
                 return true;
             }
 
             return false;
         }
 
+        /// <inheritdoc cref="OnMouseEvent"/>
         protected override bool OnMouseEvent(MouseEventData eventData)
         {
             if (IsFocused && eventData.Id == MouseEvent.MOUSE_CLICKED)
@@ -172,6 +192,7 @@ namespace Interactr.View.Controls
                 IsInEditMode = true;
                 return true;
             }
+
             return base.OnMouseEvent(eventData);
         }
     }
