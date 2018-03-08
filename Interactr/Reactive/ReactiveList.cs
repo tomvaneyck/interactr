c0adee25ce7afdafce67717240acf73c298eb9cd
Kyle Bringmans <bringmans.kyle@gmail.com>
2018-03-08 17:21:10 +0100

    added documentation

--- Interactr/Reactive/ReactiveList.cs
+++ Interactr/Reactive/ReactiveList.cs
@@ -41,7 +41,7 @@ namespace Interactr.Reactive
         public IObservable<(T Element, V Value)> ObserveEach<V>(Func<T, IObservable<V>> observableSelector)
         {
             // Take all items that are currently in the list (values and corresponding index), 
-            // aswell as all that will be added in the future.
+            // including all that will be added in the future.
             var currentContents = Observable.Zip(_contents.ToObservable(), Observable.Range(0, _contents.Count), (e, i) => (e, i));
             IObservable<(T Element, int Index)> items = currentContents.Concat(OnAdd);
 
@@ -56,24 +56,34 @@ namespace Interactr.Reactive
             );
         }
 
+
         public T this[int index]
         {
             get => _contents[index];
             set
             {
+                // Cache element in item and then replace the element in
+                // the list with value
                 T item = _contents[index];
                 _contents[index] = value;
+                // Emit an event that the elements value has changed
                 _onDelete.OnNext((item, index));
                 _onAdd.OnNext((value, index));
             }
         }
-        
+
+        /// <summary>
+        /// Add an item to a list
+        /// </summary>
         public void Add(T item)
         {
             _contents.Add(item);
             _onAdd.OnNext((item, _contents.Count-1));
         }
 
+        /// <summary>
+        /// Add multiple items to a list.
+        /// </summary>
         public void AddRange(IEnumerable<T> items)
         {
             foreach (T item in items)
@@ -82,6 +92,9 @@ namespace Interactr.Reactive
             }
         }
 
+        /// <summary>
+        /// Clears the list.
+        /// </summary>
         public void Clear()
         {
             //Clear _contents by making new list but keep items for emitting _onDelete.
@@ -98,12 +111,14 @@ namespace Interactr.Reactive
             temp.Clear();
         }
 
+        /// <inheritdoc cref="IList.Insert"/>
         public void Insert(int index, T item)
         {
             _contents.Insert(index, item);
             _onAdd.OnNext((item, index));
         }
 
+        /// <see cref="IList.Remove"/>
         public bool Remove(T item)
         {
             int index = IndexOf(item);
@@ -115,6 +130,7 @@ namespace Interactr.Reactive
             return false;
         }
 
+        /// <see cref="IList.RemoveAt"/> 
         public void RemoveAt(int index)
         {
             T item = _contents[index];
