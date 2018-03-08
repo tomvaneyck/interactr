e4fd0f74a6d4ced9fb1774861f7baedd560e082b
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-03-08 23:38:49 +0100

    Add more tests for LabelView.

--- Interactr.Tests/View/Controls/LabelViewTests.cs
+++ Interactr.Tests/View/Controls/LabelViewTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Reactive.Concurrency;
 using Interactr.View.Controls;
 using Interactr.View.Framework;
@@ -41,6 +40,9 @@ namespace Interactr.Tests.View.Controls
 
         public class TestableLabelView : LabelView
         {
+            // Count the number of times repaint is called in this labelView.
+            public int RepaintCounter { get; private set; } = 0;
+
             public bool RunOnKeyEvent(KeyEventData keyEventData)
             {
                 return OnKeyEvent(keyEventData);
@@ -102,7 +104,8 @@ namespace Interactr.Tests.View.Controls
             labelview.Height = 2;
             labelview.Width = 2;
             ui.Children.Add(labelview);
-            MouseEventData mouseEventData = new MouseEventData(MouseEvent.MOUSE_CLICKED, new Point(int.MaxValue, int.MaxValue), 1);
+            MouseEventData mouseEventData =
+                new MouseEventData(MouseEvent.MOUSE_CLICKED, new Point(int.MaxValue, int.MaxValue), 1);
 
             bool result = UIElement.HandleMouseEvent(ui, mouseEventData);
 
@@ -117,7 +120,8 @@ namespace Interactr.Tests.View.Controls
         public void MouseClickFunctionalityEventNoClick()
         {
             LabelView labelview = new LabelView();
-            MouseEventData mouseEventData = new MouseEventData(MouseEvent.NOBUTTON, new Point(int.MaxValue, int.MaxValue), 1);
+            MouseEventData mouseEventData =
+                new MouseEventData(MouseEvent.NOBUTTON, new Point(int.MaxValue, int.MaxValue), 1);
 
             bool result = UIElement.HandleMouseEvent(labelview, mouseEventData);
 
@@ -140,24 +144,59 @@ namespace Interactr.Tests.View.Controls
         {
             _labelView = new LabelViewTests.TestableLabelView();
             _scheduler = new TestScheduler();
-            _labelView.IsInEditMode = true;
         }
 
         [Test]
         public void LabelTextChangesWhenOneKeyTyped()
         {
+            _labelView.IsInEditMode = true;
             ScheduleTypeTextInLabelView("TEST");
             var expected = new[]
             {
                 ReactiveTest.OnNext(1, ""),
-                ReactiveTest.OnNext(10,"T"),
-                ReactiveTest.OnNext(20,"TE"),
-                ReactiveTest.OnNext(30,"TES"),
-                ReactiveTest.OnNext(40,"TEST")
+                ReactiveTest.OnNext(10, "T"),
+                ReactiveTest.OnNext(20, "TE"),
+                ReactiveTest.OnNext(30, "TES"),
+                ReactiveTest.OnNext(40, "TEST")
             };
             var actual = _scheduler.Start(() => _labelView.TextChanged, 0, 0, 1000).Messages;
-            
-            ReactiveAssert.AreElementsEqual(expected,actual);
+
+            ReactiveAssert.AreElementsEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestBackSpaceInLabelView()
+        {
+            _labelView.IsInEditMode = true;
+            ScheduleTypeTextInLabelView("TEST\bA");
+            var expected = new[]
+            {
+                ReactiveTest.OnNext(1, ""),
+                ReactiveTest.OnNext(10, "T"),
+                ReactiveTest.OnNext(20, "TE"),
+                ReactiveTest.OnNext(30, "TES"),
+                ReactiveTest.OnNext(40, "TEST"),
+                ReactiveTest.OnNext(50, "TES"),
+                ReactiveTest.OnNext(60, "TESA")
+            };
+            var actual = _scheduler.Start(() => _labelView.TextChanged, 0, 0, 1000).Messages;
+
+            // Assertion
+            ReactiveAssert.AreElementsEqual(expected, actual);
+        }
+
+        [Test]
+        public void BackSpaceWithNoTextInLabelViewShouldNotChangeText()
+        {
+            ScheduleTypeTextInLabelView("\b");
+            var expected = new[]
+            {
+                ReactiveTest.OnNext(1, "")
+            };
+            var actual = _scheduler.Start(() => _labelView.TextChanged, 0, 0, 1000).Messages;
+
+            // Assertion
+            ReactiveAssert.AreElementsEqual(expected, actual);
         }
 
         /// <summary>
@@ -171,8 +210,10 @@ namespace Interactr.Tests.View.Controls
             // Schedule to type a character in every 10 ticks.
             foreach (char character in text)
             {
-                KeyEventData keyEventData = new KeyEventData(KeyEvent.KEY_TYPED, KeyEvent.CHAR_UNDEFINED, character);
-                _scheduler.Schedule(TimeSpan.FromTicks(scheduleTicks), () => _labelView.RunOnKeyEvent(keyEventData));
+                KeyEventData keyEventData =
+                    new KeyEventData(KeyEvent.KEY_TYPED, KeyEvent.CHAR_UNDEFINED, character);
+                _scheduler.Schedule(TimeSpan.FromTicks(scheduleTicks),
+                    () => _labelView.RunOnKeyEvent(keyEventData));
                 scheduleTicks += 10;
             }
         }
