613d061cb3e77755b2c35c4cea3a84fbabbb2a2e
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-03-09 10:01:02 +0100

    Create an activation bar viewmodel.

---
+++ Interactr/ViewModel/ActivationBarViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Interactr.Model;
+
+namespace Interactr.ViewModel
+{
+    /// <summary>
+    /// A viewModel for the activationBar.
+    /// the activation bar is immutable.
+    /// </summary>
+    /// <remarks>There is no underlying model for the activationBar viewModel.</remarks>
+    public class ActivationBarViewModel
+    {
+        #region party
+
+        public Party Party { get; }
+
+        #endregion
+
+        #region tick
+
+        public int StartTick { get; }
+
+        #endregion
+
+        #region message tick
+
+        public int MessageTick { get; }
+
+        #endregion
+
+        public ActivationBarViewModel(Party party, int startTick, int messageTick)
+        {
+            Party = party;
+            StartTick = startTick;
+            MessageTick = messageTick;
+        }
+    }
+}
