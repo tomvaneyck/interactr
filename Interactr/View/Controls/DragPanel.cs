7146bea92db63edbb710745dfb88e87f9ef06aa3
Tom Van Eyck <tom.vaneyck@telenet.be>
2018-03-09 10:18:27 +0100

    Added drag panel
    Also fixed another focus bug

---
+++ Interactr/View/Controls/DragPanel.cs
@@ -0,0 +1,129 @@
+﻿using Interactr.View.Framework;
+using Interactr.Window;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.View.Controls
+{
+    /// <summary>
+    /// This is a panel that enables some elements to be dragged around.
+    /// </summary>
+    class DragPanel : UIElement
+    {
+        private Point _previousCursorPosition;
+
+        public DragPanel()
+        {
+            // Update layout when the width or height of this panel is changed.
+            Observable.Merge(
+                WidthChanged.Select(_ => Unit.Default),
+                HeightChanged.Select(_ => Unit.Default)
+            ).Subscribe(_ => UpdateLayout());
+
+            // Update layout when a child changes its preferred width or height.
+            Observable.Merge(
+                Children.ObserveEach(child => child.PreferredWidthChanged),
+                Children.ObserveEach(child => child.PreferredHeightChanged)
+            ).Subscribe(_ => UpdateLayout());
+        }
+
+        /// <see cref="UIElement.OnMouseEvent(MouseEventData)"/>
+        protected override bool OnMouseEvent(MouseEventData eventData)
+        {
+            if (eventData.Id == MouseEvent.MOUSE_DRAGGED) {
+                MouseDragEventData dragEventData = new MouseDragEventData(
+                    eventData.MousePosition.X - _previousCursorPosition.X,
+                    eventData.MousePosition.Y - _previousCursorPosition.Y
+                );
+                ApplyDragToFocusedElement(dragEventData);
+                _previousCursorPosition = eventData.MousePosition;
+                return true;
+            }
+            else
+            {
+                return base.OnMouseEvent(eventData);
+            }
+        }
+
+        /// <see cref="UIElement.OnMouseEventPreview(MouseEventData)"/>
+        protected override bool OnMouseEventPreview(MouseEventData eventData)
+        {
+            if (eventData.Id == MouseEvent.MOUSE_PRESSED)
+            {
+                _previousCursorPosition = eventData.MousePosition;
+            }
+            return base.OnMouseEventPreview(eventData);
+        }
+
+        /// <summary>
+        /// Aplly the drag data to the focused element.
+        /// </summary>
+        /// <remarks>
+        /// Only apply the data if the focused element is a descendant.
+        /// </remarks>
+        /// <param name="dragEventData">The drag data.</param>
+        private void ApplyDragToFocusedElement(MouseDragEventData dragEventData)
+        {
+            UIElement dragElement = FocusedElement.WalkToRoot().FirstOrDefault((element) => element.Parent == this);
+
+            if (dragElement != null)
+            {
+                Point newPosition = new Point(
+                    (int)(dragElement.Position.X + dragEventData.DeltaX),
+                    (int)(dragElement.Position.Y + dragEventData.DeltaY)
+                );
+
+                if (IsValidPosition(dragElement, newPosition))
+                {
+                    dragElement.Position = newPosition;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check if the given position + width of given element is inside this dragPanel.
+        /// </summary>
+        /// <param name="element">The given element.</param>
+        /// <param name="position">The given position.</param>
+        /// <returns></returns>
+        private bool IsValidPosition(UIElement element, Point position)
+        {
+            return (
+                position.X + element.Width < Width &&
+                position.X >= 0 &&
+                position.Y + element.Height < Height &&
+                position.Y >= 0
+            );
+        }
+
+        /// <summary>
+        /// Update the layout of all the children.
+        /// </summary>
+        private void UpdateLayout()
+        {
+            foreach (UIElement child in Children)
+            {
+                UpdateLayout(child);
+            }
+        }
+
+        /// <summary>
+        /// Update the layout of the given child.
+        /// </summary>
+        /// <remarks>
+        /// Change the width of the child if it wants to.
+        /// </remarks>
+        /// <param name="child">The given child.</param>
+        private void UpdateLayout(UIElement child)
+        {
+            child.Width = child.PreferredWidth;
+            child.Height = child.PreferredHeight;
+        }
+    }
+}
---
+++ Interactr/View/Framework/MouseDragEventData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactr.View.Framework
+{
+    /// <summary>
+    /// Information about a mouse drag event.
+    /// </summary>
+    class MouseDragEventData
+    {
+        /// <summary>
+        /// The distance over which the mouse is dragged.
+        /// </summary>
+        public double DeltaX { get; }
+        public double DeltaY { get; }
+
+        public MouseDragEventData(int deltaX, int deltaY)
+        {
+            DeltaX = deltaX;
+            DeltaY = deltaY;
+        }
+    }
+}
