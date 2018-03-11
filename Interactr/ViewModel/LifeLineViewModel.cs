9bd2c2fbe9dd18f3c5823c2b6ba33bef5108c20a
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-03-11 14:02:10 +0100

    Add comments to life line view model.

--- Interactr/ViewModel/LifeLineViewModel.cs
+++ Interactr/ViewModel/LifeLineViewModel.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Interactr.ViewModel
 {
+    /// <summary>
+    /// A life line view model for the lifeline in sequence diagram.
+    /// </summary>
     public class LifeLineViewModel
     {
+        /// <summary>
+        /// The message stack view model associated with the lifeline.
+        /// </summary>
         public MessageStackViewModel MessageStackVM { get; }
+
+        /// <summary>
+        /// The party view model associated with the lifeline.
+        /// </summary>
         public PartyViewModel PartyVM { get; }
 
         public LifeLineViewModel(MessageStackViewModel messageStackViewModel, PartyViewModel party)
