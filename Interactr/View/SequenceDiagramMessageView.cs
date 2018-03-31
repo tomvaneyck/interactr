5181e3f163063e81f8965e629b32d1db8c1ce795
Wouter <wouterdekrsmckr@gmail.com>
2018-03-31 14:31:34 +0200

    Reduce code duplication

--- Interactr/View/SequenceDiagramMessageView.cs
+++ Interactr/View/SequenceDiagramMessageView.cs
@@ -40,45 +40,51 @@ namespace Interactr.View
 
             // Put the arrow starting point on the sender activation bar.
             ObserveActivationBarPosition(vm => vm.SenderActivationBarChanged)
-                .Select(activationBarView =>
-                {
-                    SequenceDiagramView parent = (SequenceDiagramView)Parent;
-                    Point anchorPoint = new Point(
-                        0,
-                        (ViewModel.Tick - activationBarView.ViewModel.StartTick) * activationBarView.TickHeight
-                    );
-                    Point translatedAnchorPoint = activationBarView.TranslatePointTo(parent, anchorPoint);
-                    int xOffset = translatedAnchorPoint.X < _arrow.EndPoint.X ? activationBarView.Width : 0;
-                    return translatedAnchorPoint + new Point(xOffset, 0);
-                }).Subscribe(newStartPoint => _arrow.StartPoint = newStartPoint);
+                .Select(activationBarView => GetArrowAnchorPoint(activationBarView, _arrow.EndPoint))
+                .Subscribe(newStartPoint => _arrow.StartPoint = newStartPoint);
 
             // Put the arrow ending point on the receiver activation bar.
             ObserveActivationBarPosition(vm => vm.ReceiverActivationBarChanged)
-                .Select(activationBarView =>
-                {
-                    SequenceDiagramView parent = (SequenceDiagramView)Parent;
-                    Point anchorPoint = new Point(
-                        0,
-                        (ViewModel.Tick - activationBarView.ViewModel.StartTick) * activationBarView.TickHeight
-                    );
-                    Point translatedAnchorPoint = activationBarView.TranslatePointTo(parent, anchorPoint);
-                    int xOffset = translatedAnchorPoint.X < _arrow.StartPoint.X ? activationBarView.Width : 0;
-                    return translatedAnchorPoint + new Point(xOffset, 0);
-                }).Subscribe(newEndPoint => _arrow.EndPoint = newEndPoint);
+                .Select(activationBarView => GetArrowAnchorPoint(activationBarView, _arrow.StartPoint))
+                .Subscribe(newEndPoint => _arrow.EndPoint = newEndPoint);
 
-            // Put the label central under the arrow.
+            // Put the label under the arrow.
             ViewModelChanged.ObserveNested(vm => vm.LabelChanged).Subscribe(label => _label.Text = label);
             Observable.CombineLatest(
                 _arrow.StartPointChanged,
                 _arrow.EndPointChanged
             ).Subscribe(p =>
             {
+                // Get the leftmost point
                 Point start = p[0].X < p[1].X ? p[0] : p[1];
+                // Get the rightmost point
                 Point end = p[0].X > p[1].X ? p[0] : p[1];
+                // Get the vector from the leftmost to the rightmost point.
                 Point diff = end - start;
+                // Start the text at a third of the distance between the points. Looks good enough for now.
                 Point textPos = start + new Point(diff.X / 3, diff.Y / 3);
                 MarginsProperty.SetValue(_label, new Margins(textPos.X, textPos.Y));
             });
+        }
+
+        /// <summary>
+        /// Calculate the point on the diagram where the arrow should start/stop.
+        /// </summary>
+        /// <param name="bar">The activation bar that the arrow should attach to.</param>
+        /// <param name="otherSideOfArrow">The point that defines the other side of the arrow.</param>
+        /// <returns>A point on the diagram view.</returns>
+        private Point GetArrowAnchorPoint(ActivationBarView bar, Point otherSideOfArrow)
+        {
+            Point anchorPointOnBar = new Point(
+                0,
+                (ViewModel.Tick - bar.ViewModel.StartTick) * bar.TickHeight
+            );
+            SequenceDiagramView parent = (SequenceDiagramView)Parent;
+            Point anchorPointOnDiagram = bar.TranslatePointTo(parent, anchorPointOnBar);
+
+            // Choose left or right side of bar based on which side the arrow is going.
+            int xOffset = anchorPointOnDiagram.X > otherSideOfArrow.X ? 0 : bar.Width;
+            return anchorPointOnDiagram + new Point(xOffset, 0);
         }
 
         private IObservable<ActivationBarView> ObserveActivationBarPosition(Func<SequenceDiagramMessageViewModel, IObservable<ActivationBarViewModel>> barSelector)
