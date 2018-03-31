f05d175e10ef4624f72695a63dc6e5f8c4523206
Wouter <wouterdekrsmckr@gmail.com>
2018-03-31 14:34:43 +0200

    Moved classes to seperate files

--- Interactr/View/SequenceDiagramView.cs
+++ Interactr/View/SequenceDiagramView.cs
@@ -145,34 +145,4 @@ namespace Interactr.View
             return base.OnMouseEventPreview(eventData);
         }
     }
-
-    /// <summary>
-    /// The sequence diagram column view.
-    /// Contains the party view and a corresponding lifeline.
-    /// </summary>
-    public class SequenceDiagramColumnView : AnchorPanel
-    {
-        public PartyView PartyView { get; }
-        public LifeLineView LifeLineView { get; }
-
-        public SequenceDiagramColumnView(SequenceDiagramView parent, PartyViewModel partyVM)
-        {
-            // Create the party view and add it to this column view.
-            PartyView = new PartyView
-            {
-                ViewModel = partyVM
-            };
-            AnchorsProperty.SetValue(PartyView, Anchors.Left | Anchors.Top | Anchors.Right);
-            Children.Add(PartyView);
-
-            // Create the lifeline view and add it to this column view.
-            LifeLineView = new LifeLineView();
-            MarginsProperty.SetValue(LifeLineView, new Margins(0, PartyView.PreferredHeight, 0, 0));
-            Children.Add(LifeLineView);
-            
-            // Setup viewmodel subscription.
-            parent.ViewModelChanged.Select(vm => vm.StackVM)
-                .Subscribe(stackVM => LifeLineView.ViewModel = stackVM.CreateLifeLineForParty(partyVM));
-        }
-    }
 }
