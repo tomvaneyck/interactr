cccfe14d8851d38f56c5a17a9ab7016692b61e36
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-04-16 14:33:47 +0200

    Add messageNumber property.

--- Interactr/ViewModel/CommunicationDiagramMessageViewModel.cs
+++ Interactr/ViewModel/CommunicationDiagramMessageViewModel.cs
@@ -1,9 +1,17 @@
-﻿using Interactr.Model;
+﻿using System;
+using Interactr.Model;
 
 namespace Interactr.ViewModel
 {
     public class CommunicationDiagramMessageViewModel : MessageViewModel
     {
+
+        /// <summary>
+        /// The message number of the message on it's own nesting level.
+        /// For example, a message has an 
+        /// </summary>
+        private int MessageNumber {get; set; }
+
         public CommunicationDiagramMessageViewModel(Message message) : base(message)
         {
         }
