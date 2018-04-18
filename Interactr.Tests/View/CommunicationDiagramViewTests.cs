12818f57db0a77fa3144bb7f14166c690788f6e9
Unknown <eleonora.vangenechten@student.kuleuven.be>
2018-04-18 16:18:56 +0200

    Add comments.

--- Interactr.Tests/View/CommunicationDiagramViewTests.cs
+++ Interactr.Tests/View/CommunicationDiagramViewTests.cs
@@ -34,22 +34,20 @@ namespace Interactr.Tests.View
                 ReactiveTest.OnNext(10, true)
             };
 
-            // Check if the visibility is as expected.
             var actual = _scheduler.Start(() => _communicationDiagramView.IsVisibleChanged, 0, 0, 1000).Messages;
 
+            // Assert that the visibility changed as expected.
             ReactiveAssert.AreElementsEqual(expected, actual);
         }
-
 
         [Test]
         public void AddPartyOnDoubleMouseClick()
         {
-           _communicationDiagramView.RunOnMouseEvent(new MouseEventData(Window.MouseEvent.MOUSE_CLICKED,new Point(0,0),2)));
+            _communicationDiagramView.RunOnMouseEvent(new MouseEventData(Window.MouseEvent.MOUSE_CLICKED, new Point(0, 0), 2)));
+
+            // Assert the amount of parties has increased from 0 to 1.
             Assert.AreEqual(_communicationDiagramView.PartyViews.Count, 1);
         }
-
-        [Test]
-        public void 
 
         private class TestableCommunicationDiagramView : CommunicationDiagramView
         {
