44343cd6361d4a2ebcfb8877eab8c3fbed97f4b6
Tom Van Eyck <tom.vaneyck@telenet.be>
2018-04-19 13:48:50 +0200

    Added bash script

--- Generate documentation.ps1
+++ Generate documentation.ps1
@@ -1,5 +1,13 @@
-﻿# Download tools
-echo "Downloading necessary tools..."
+﻿# Check if docfx is already generated, otherwise exit
+if (!(Test-Path .\Documentation\docfx.json)) {
+    Write-Error "docfx.json was not found. Are you sure you are running this script from the root directory and have pulled the right files?"
+    [void](Read-Host 'Press Enter to exit...')
+    exit
+}
+
+
+# Download tools
+Write-Output "Downloading necessary tools..."
 
 $url = "https://github.com/dotnet/docfx/releases/download/v2.34/docfx.zip"
 $outputdir = ".\Documentation\Tools\"
@@ -13,7 +21,7 @@ Invoke-WebRequest -Uri $url -OutFile $output
 
 
 # Unzip tools
-echo "" "Unpacking tools..."
+Write-Output "" "Unpacking tools..."
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $zipfile = $output
@@ -26,28 +34,23 @@ Expand-Archive -Force $zipfile -DestinationPath $output
 Set-Alias -Name docfx -Value "$((Get-Item -Path ./).FullName)\Documentation\Tools\docfx\docfx.exe"
 
 
-# Check if docfx is already generated, otherwise generate
-if (!(Test-Path .\Documentation\docfx.json)) {
-    Write-Error "docfx.json was not found. Are you sure you are running this script from the root directory and have pulled the right files?"
-    pause
-    [void](Read-Host 'Press Enter to exit...')
-}
-
 # Initialize docfx
-echo "--------------------------" "" "Initializing docfx framework" ""
+Write-Output "--------------------------" "" "Initializing docfx framework..." ""
 docfx init -q -o Documentation
 
+
 # Force build the metadata
-echo "--------------------------" "" "Force building the metadata..." ""
+Write-Output "--------------------------" "" "Force building the metadata..." ""
 docfx metadata .\Documentation\docfx.json -f
 
 
 # Force build the documentation
-echo "--------------------------" "" "Force building the documentation..." ""
+Write-Output "--------------------------" "" "Force building the documentation..." ""
 docfx build .\Documentation\docfx.json -f
 
+
 # Serve the documentation if wanted by the user
-echo "--------------------------"
+Write-Output "--------------------------"
 
 $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
 $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
