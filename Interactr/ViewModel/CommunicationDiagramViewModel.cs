3ae781d8b2f69d17811686598ada4a31641d48ad
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-05-10 00:21:22 +0200

    Remove superfluous dependencies.

--- Interactr/ViewModel/CommunicationDiagramViewModel.cs
+++ Interactr/ViewModel/CommunicationDiagramViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Interactr.Model;
 using System.Reactive.Linq;
 using Interactr.Reactive;
