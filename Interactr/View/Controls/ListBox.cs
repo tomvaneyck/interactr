356a3903eaf0bbc1213f09fb8acfd59fcc5c7d9c
Wouter <wouterdekrsmckr@gmail.com>
2018-05-19 21:35:49 +0200

    Started work on a listbox implementation

---
+++ Interactr/View/Controls/ListBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interactr.View.Framework;
+
+namespace Interactr.View.Controls
+{
+    public class ListBox<T> : StackPanel
+    {
+        private Button _moveUpButton;
+        private Button _moveDownButton;
+        private Button _deleteButton;
+        private ListView<T> _listBox;
+
+        public ListBox(Func<T, UIElement> viewFactory)
+        {
+            _moveUpButton = new Button
+            {
+                Label = "Move up"
+            };
+            _moveDownButton = new Button
+            {
+                Label = "Move down"
+            };
+            _deleteButton = new Button
+            {
+                Label = "Delete"
+            };
+
+            this.Children.Add(new StackPanel
+            {
+                Children =
+                {
+                    _moveUpButton,
+                    _moveDownButton,
+                    _deleteButton
+                },
+                StackOrientation = Orientation.Horizontal
+            });
+
+            _listBox = new ListView<T>(viewFactory);
+            this.Children.Add(_listBox);
+        }
+    }
+}
---
+++ Interactr/View/Controls/ListView.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interactr.Reactive;
+using Interactr.View.Framework;
+using Interactr.ViewModel;
+using Interactr.Window;
+
+namespace Interactr.View.Controls
+{
+    public class ListView<T> : StackPanel
+    {
+        #region ItemsSource
+
+        private readonly ReactiveProperty<IReadOnlyReactiveList<T>> _itemsSource = new ReactiveProperty<IReadOnlyReactiveList<T>>();
+
+        public IReadOnlyReactiveList<T> ItemsSource
+        {
+            get => _itemsSource.Value;
+            set => _itemsSource.Value = value;
+        }
+
+        public IObservable<IReadOnlyReactiveList<T>> ItemsSourceChanged => _itemsSource.Changed;
+
+        #endregion
+        
+        #region SelectedItem
+
+        private readonly ReactiveProperty<T> _selectedItem = new ReactiveProperty<T>();
+
+        public T SelectedItem
+        {
+            get => _selectedItem.Value;
+            set => _selectedItem.Value = value;
+        }
+
+        public IObservable<T> SelectedItemChanged => _selectedItem.Changed;
+
+        #endregion
+
+        #region SelectedIndex
+
+        private readonly ReactiveProperty<int> _selectedIndex = new ReactiveProperty<int>();
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex.Value;
+            set => _selectedIndex.Value = value;
+        }
+
+        public IObservable<int> SelectedIndexChanged => _selectedIndex.Changed;
+
+        #endregion
+
+        private ItemContainer _selectedContainer;
+
+        public ListView(Func<T, UIElement> viewFactory)
+        {
+            var elements = ItemsSourceChanged.CreateDerivedListBinding(e => new ItemContainer(e, viewFactory(e))).ResultList;
+            elements.OnAdd.Subscribe(e => Children.Insert(e.Index, e.Element));
+            elements.OnDelete.Subscribe(e => this.Children.RemoveAt(e.Index));
+
+            // When a child is clicked, mark it as selected
+            this.Children.ObserveEach(e => e.MouseEventOccured).Subscribe(e =>
+            {
+                if (e.Value.Id == MouseEvent.MOUSE_PRESSED)
+                {
+                    ItemContainer selectedContainer = (ItemContainer)e.Element;
+                    selectedContainer.IsSelected = true;
+                    e.Value.IsHandled = true;
+                }
+            });
+
+            // When a child becomes selected, unselect all other items
+            this.Children.ObserveEach(e => ((ItemContainer)e).IsSelectedChanged).Subscribe(e =>
+            {
+                if (_selectedContainer != null)
+                {
+                    _selectedContainer.IsSelected = false;
+                }
+                _selectedContainer = (ItemContainer)e.Element;
+            });
+
+            // When the selected element is deleted, select a neighbour or nothing
+
+            // When the selected element is moved, update the selected index
+        }
+
+        private class ItemContainer : AnchorPanel
+        {
+            public T Item { get; }
+            
+            #region IsSelected
+
+            private readonly ReactiveProperty<bool> _isSelected = new ReactiveProperty<bool>();
+
+            public bool IsSelected
+            {
+                get => _isSelected.Value;
+                set => _isSelected.Value = value;
+            }
+
+            public IObservable<bool> IsSelectedChanged => _isSelected.Changed;
+
+            #endregion
+            
+            public ItemContainer(T item, UIElement view)
+            {
+                Item = item;
+
+                MarginsProperty.SetValue(view, new Margins(3, 3, 3, 3));
+                this.Children.Add(view);
+
+                IsSelectedChanged.Subscribe(_ => Repaint());
+            }
+
+            public override void PaintElement(Graphics g)
+            {
+                g.FillRectangle(IsSelected ? Brushes.LightBlue : Brushes.White, 0, 0, Width, Height);
+                g.DrawRectangle(Pens.Black, 0, 0, Width-1, Height-1);
+            }
+        }
+    }
+}
