e9ad6c2f143bac1d7b80a7b17e486e5b8ae2ace3
Wouter <wouterdekrsmckr@gmail.com>
2018-05-21 13:00:06 +0200

    Implemented ListBox buttons

--- Interactr/View/Controls/ListBox.cs
+++ Interactr/View/Controls/ListBox.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Interactr.Reactive;
 using Interactr.View.Framework;
 
 namespace Interactr.View.Controls
 {
     public class ListBox<T> : StackPanel
     {
+        #region ItemsSource
+
+        private readonly ReactiveProperty<ReactiveList<T>> _itemsSource = new ReactiveProperty<ReactiveList<T>>();
+
+        public ReactiveList<T> ItemsSource
+        {
+            get => _itemsSource.Value;
+            set => _itemsSource.Value = value;
+        }
+
+        public IObservable<ReactiveList<T>> ItemsSourceChanged => _itemsSource.Changed;
+
+        #endregion
+
         private Button _moveUpButton;
         private Button _moveDownButton;
         private Button _deleteButton;
@@ -20,14 +35,26 @@ namespace Interactr.View.Controls
             {
                 Label = "Move up"
             };
+            _moveUpButton.OnButtonClick.Subscribe(_ =>
+            {
+                ItemsSource.MoveByIndex(_listBox.SelectedIndex, _listBox.SelectedIndex - 1);
+            });
             _moveDownButton = new Button
             {
                 Label = "Move down"
             };
+            _moveDownButton.OnButtonClick.Subscribe(_ =>
+            {
+                ItemsSource.MoveByIndex(_listBox.SelectedIndex, _listBox.SelectedIndex + 1);
+            });
             _deleteButton = new Button
             {
                 Label = "Delete"
             };
+            _moveUpButton.OnButtonClick.Subscribe(_ =>
+            {
+                ItemsSource.RemoveAt(_listBox.SelectedIndex);
+            });
 
             this.Children.Add(new StackPanel
             {
@@ -41,6 +68,7 @@ namespace Interactr.View.Controls
             });
 
             _listBox = new ListView<T>(viewFactory);
+            this.ItemsSourceChanged.Subscribe(list => _listBox.ItemsSource = list);
             this.Children.Add(_listBox);
         }
     }
