6b9665187a895def7271baa3abc61970324d2a72
EleonoraVG <eleonora.vangenechten@student.kuleuven.be>
2018-05-24 16:52:36 +0200

    fix probles of fix problems

--- Interactr/ViewModel/MessageViewModel.cs
+++ Interactr/ViewModel/MessageViewModel.cs
@@ -143,7 +143,6 @@ namespace Interactr.ViewModel
                     }
 
                     newLabel += ")";
-                    Label.Label = newLabel;
                 });
             }
         }
